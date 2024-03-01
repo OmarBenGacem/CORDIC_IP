36865bfde3a26b804377081343dcb2625686dbdc
omarbengacem <omar.ben-gacem20@imperial.ac.uk>
2024-03-01 16:29:54 +0000

    added more ips

---
+++ eight_bit_fixed_gt/eight_bit_fixed_gt_inst.v
@@ -0,0 +1,6 @@
+eight_bit_fixed_gt	eight_bit_fixed_gt_inst (
+	.dataa ( dataa_sig ),
+	.datab ( datab_sig ),
+	.aeb ( aeb_sig ),
+	.agb ( agb_sig )
+	);
---
+++ eight_bit_int_add/eight_bit_int_add_inst.v
@@ -0,0 +1,5 @@
+eight_bit_int_add	eight_bit_int_add_inst (
+	.dataa ( dataa_sig ),
+	.datab ( datab_sig ),
+	.result ( result_sig )
+	);
