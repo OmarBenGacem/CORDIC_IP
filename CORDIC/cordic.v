319df7acc2b31a2efccd69b58ff364a498dc6ef6
omarbengacem <omar.ben-gacem20@imperial.ac.uk>
2024-03-10 10:08:00 +0000

    cordic changes

--- CORDIC/cordic.v
+++ CORDIC/cordic.v
@@ -5,78 +5,82 @@ module CORDIC(clk, rst, clk_en, angle_float, result, done);
 //Data Parameters
 parameter FLOAT_DATA_WIDTH = 32;
 parameter CORDIC_ADDRESS_WIDTH = 4;
-parameter INTEGER_WIDTH = 4;
-parameter FRACTIONAL_WIDTH = 20;
+parameter INTEGER_WIDTH = 1;
+parameter FRACTIONAL_WIDTH = 21;
 parameter CORDIC_DATA_WIDTH = INTEGER_WIDTH + FRACTIONAL_WIDTH;
 
 
 
 //Latency Parameters
-parameter CONVERSION_LATANCY = {{COUNTER_WIDTH}'b1};
+parameter CONVERSION_LATANCY = 4;
+parameter CONVERSION_LATANCY_REV = 4;
 
 //Control Parameters
-parameter CONVERSION_COUNTER_WIDTH = 3;
 parameter CORDIC_COUNTER_WIDTH = 4;
-parameter CORDIC_DEPTH = 8;
+parameter CORDIC_DEPTH = 11;
 parameter COUNTER_WIDTH = 10;
 parameter STATE_WIDTH = 5;
 
-parameter IDLE = {STATE_WIDTH{'b0000}}
-parameter CONVERTING = {STATE_WIDTH{'b0010}}
-parameter CORDIC_MAIN = {STATE_WIDTH{'b0001}}
+// parameter IDLE = {STATE_WIDTH{'b0000}}
+// parameter CONVERTING = {STATE_WIDTH{'b0010}}
+// parameter CORDIC_MAIN = {STATE_WIDTH{'b0001}}
+// parameter CONVERTING_BACK = 4'b0011;
+// parameter DONE = {STATE_WIDTH{'b0001}}
+
+parameter IDLE = 4'b0000;
+parameter CONVERTING = 4'b0010;
+parameter CORDIC_MAIN = 4'b0111;
 parameter CONVERTING_BACK = 4'b0011;
-parameter DONE = {STATE_WIDTH{'b0001}}
+parameter DONE = 4'b0001;
 
 
 
 
 
-//IO
-input                               
+//IO                     
 input                               clk;
 input                               rst;
 input                               clk_en;
 input      [FLOAT_DATA_WIDTH - 1:0] angle_float;
-output                              done;
+output reg                          done;
 output reg [FLOAT_DATA_WIDTH - 1:0] result;
 
 
 //Control Registers
 reg  [COUNTER_WIDTH - 1:0]              counter_max;
 reg  [CORDIC_COUNTER_WIDTH - 1: 0]      cordic_counter;
-reg  [CONVERSION_COUNTER_WIDTH - 1: 0]  conversion_counter;
 reg  [STATE_WIDTH - 1: 0]               state;
 reg                                     start_conversion;       
+reg                                     delay_reset;
 
 
 //Data Registers (and ROM)
-reg  [CORDIC_DATA_WIDTH-1:0]      CORDIC_shifts [2**CORDIC_ADDRESS_WIDTH-1: 0]; //values as fixed point
+reg  [CORDIC_DATA_WIDTH - 1:0]    CORDIC_shifts [2**CORDIC_ADDRESS_WIDTH-1: 0]; //values as fixed point
 reg  [CORDIC_DATA_WIDTH - 1: 0]   target;
 reg  [CORDIC_DATA_WIDTH - 1: 0]   working_angle;
 reg  [CORDIC_DATA_WIDTH - 1: 0]   x; 
 reg  [CORDIC_DATA_WIDTH - 1: 0]   y;
-reg  [CORDIC_DATA_WIDTH - 1: 0]   shifted_y;
-reg  [CORDIC_DATA_WIDTH - 1: 0]   shifted_x;
+wire [CORDIC_DATA_WIDTH - 1: 0]   shifted_y;
+wire [CORDIC_DATA_WIDTH - 1: 0]   shifted_x;
+wire [CORDIC_DATA_WIDTH - 1: 0]   shift_value;
 
+wire [CORDIC_DATA_WIDTH - 1: 0]   angle;
 wire [CORDIC_DATA_WIDTH - 1: 0]   new_x;
 wire [CORDIC_DATA_WIDTH - 1: 0]   new_y;
 wire [CORDIC_DATA_WIDTH - 1: 0]   new_angle;
-wire                              sign_result;
-wire [FLOAT_DATA_WIDTH - 1:0]     angle;
 wire [FLOAT_DATA_WIDTH - 1:0]     result_flt;
-wire                              conversion_done;
 wire                              angle_greater_target;
-wire                              a_equal_x_lt;
-wire                              a_greater_than_x;
-wire                              a_less_than_x;
+wire                              angle_equal_target;
+wire                              counter_done;      
+
 
 //module convert_fp_fixed (aclr, clk_en, clock, dataa, result)/* synthesis synthesis_clearbox = 1 */;
-FP_Convert converter_fp_fix (
+fp_convert converter_fp_fix (
 
     .clock      ( clk ),
     .aclr       ( rst ),
     .clk_en     ( start_conversion ),
-    .dataa      ( angle_float ),
+    .dataa      ( {1'b0, angle_float[FLOAT_DATA_WIDTH - 2:0]} ),
     .result     ( angle )
 
 );
@@ -106,29 +110,28 @@ delay stopper (
 
 Fixed_Add_Sub addsub_x (
 	.dataa ( x ),
-	.datab ( shifted_x ),
-    .addsub ( (a_greater_than_x) ), //1 for add, 0 for sub
+	.datab ( shifted_y ),
+    .add_sub ( (angle_greater_target) ), //1 for add, 0 for sub
 	.result ( new_x )
 );
 
 Fixed_Add_Sub addsub_y (
 	.dataa ( y ),
-	.datab ( shifted_y ),
-    .addsub ( !(a_greater_than_x) ), //1 for add, 0 for sun
+	.datab ( shifted_x ),
+    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
 	.result ( new_y )
 );
 
-
 Fixed_Add_Sub addsub_angle (
-	.dataa ( angle ),
-	.datab ( CORDIC_shifts[cordic_counter] ),
-    .addsub ( !(a_greater_than_x) ), //1 for add, 0 for sun
-	.result ( new_y )
+	.dataa ( working_angle ),
+	.datab ( shift_value ),
+    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
+	.result ( new_angle )
 );
 
 Fixed_Point_gt	angle_gt (
-	.dataa ( angle ),
-	.datab ( working_angle ),
+	.dataa ( working_angle ),
+	.datab ( target ),
 	.aeb ( angle_equal_target ),
 	.agb ( angle_greater_target )
 );
@@ -136,13 +139,21 @@ Fixed_Point_gt	angle_gt (
 
 initial begin
 
-    working_angle <= {{CORDIC_DATA_WIDTH}'b0};
-    y <= {{CORDIC_DATA_WIDTH}'b0};
+    // working_angle <= {{CORDIC_DATA_WIDTH}'b0};
+    // y <= {{CORDIC_DATA_WIDTH}'b0};
+    // cordic_counter <= {{CORDIC_COUNTER_WIDTH}'b0};
+    // state <= {{STATE_WIDTH}'b0};
+
+    working_angle <= 22'b0;
+    y <= 22'b0;
+    cordic_counter <= 4'b0;
+    state <= IDLE;
+    result <= 22'b0;
     x <= 22'b0100110110111010011101;
-    cordic_counter <= {{CORDIC_COUNTER_WIDTH}'b0};
-    state <= {{STATE_WIDTH}'b0};
     start_conversion <= 1'b0;
     counter_max <= CONVERSION_LATANCY;
+    done <= 1'b0;
+    target <= 22'b0;
 
 
 
@@ -156,84 +167,108 @@ initial begin
     CORDIC_shifts[4'b0111] <= 22'b0000000011111111111111;
     CORDIC_shifts[4'b1000] <= 22'b0000000001111111111111;
     CORDIC_shifts[4'b1001] <= 22'b0000000000111111111111;
+    CORDIC_shifts[4'b1010] <= 22'b0000000000011111111111;
 
 
 end
 
-always @(posedge clk) begin
 
-    //continuous signals
-    shifted_y <= y >>> cordic_counter;
-    shifted_x <= x >>> cordic_counter;
+//combinational logic signals
+assign shift_value =  CORDIC_shifts[cordic_counter];
+assign shifted_y = y >>> cordic_counter;
+assign shifted_x = x >>> cordic_counter;
 
-    
+//clock syncronous signals
+always @(posedge clk) begin    
     
     if (rst) begin
         state  <= IDLE;
     end
 
-    case(state):
+    case(state)
 
-        IDLE: begin
-
+        IDLE: begin //0000
+            //do nothing unless clock begins
+            if (done) done <= 1'b0; // reset done signal
             if (clk_en) begin
                 //Shorting out 45 and 0 degrees
                 if (angle_float == 32'b00111111010010010000111111011000 || angle_float == 32'b0) begin
-                    state <= DONE
-                    if (angle_float == 32'b0) result <= 32'b00111111100000000000000000000000; else result <= 00111111001101010000010011110011;  
+                    //short out 45 degrees (and 0)
+                    state <= DONE;
+                    if (angle_float == 32'b0) result <= 32'b00111111100000000000000000000000; else result <= 32'b00111111001101010000010011110011;  
                 end else begin
                     state <= CONVERTING;
                     start_conversion <= 1'b1;
                     delay_reset <= 1'b1; //start delay block
+                    cordic_counter <= 4'b0;
+                    x <= 22'b0100110110111010011101; //reset value of x
+                    working_angle <= 22'b0;
+                    y <= 22'b0;
+                    result <= 22'b0;
+                    done <= 1'b0;
+                    target <= 22'b0;
                     
                 end
-            end else begin
-                //assign initial values
-                x <= 22'b0100110110111010011101;
-                cordic_counter <= 4'b0;
-
-            end
+            end 
 
         end
 
 
-        CONVERTING begin
+        CONVERTING: begin //0010
             delay_reset <= 1'b0;
-            if (counter_done) state <= CORDIC_MAIN; start_conversion <= 1'b0; working_angle <= angle; 
+            if (counter_done) 
+            begin state <= CORDIC_MAIN; 
+            start_conversion <= 1'b0; 
+            target <= angle;
+            end
         
-        end:
+        end
 
 
-        CORDIC_MAIN: begin
+        CORDIC_MAIN: begin //0111
 
             if (cordic_counter == CORDIC_DEPTH || angle_equal_target) begin
-                state <= DONE;
+                delay_reset <= 1'b1; //start delay block
+                start_conversion <= 1'b1;
+                state <= CONVERTING_BACK;
             end else begin
-                
-                x <= x_new;
-                y <= y_new;
-                angle <= angle_new;
-                cordic_counter = cordic_counter + 1;
+                x <= new_x;
+                y <= new_y;
+                working_angle <= new_angle;
+                cordic_counter <= cordic_counter + 1;
+
+                //preempting activating the delays
+                if (cordic_counter == CORDIC_DEPTH - 1) begin
+                    delay_reset <= 1'b1; //start delay block
+                    start_conversion <= 1'b1;
+                    counter_max <= CONVERSION_LATANCY_REV;
+                end
             end
 
         end
 
-        CONVERTING_BACK: begin
+        CONVERTING_BACK: begin //0011
+            counter_max <= CONVERSION_LATANCY_REV;
             start_conversion <= 1'b1;
             delay_reset <= 1'b0; //start delay block
-            if (counter_done) state <= DONE; start_conversion <= 1'b0; 
+            if (counter_done) begin 
+                state <= DONE; 
+                start_conversion <= 1'b0; 
+                result <= {1'b0, result_flt[FLOAT_DATA_WIDTH - 2:0]}; 
+            end
 
     
         end
 
-        DONE: begin
+        DONE: begin //0001
             //need to convert back to floating point
-            result <= result_flt;
-            done <= 1'b1;
-            result <= x;
-            if (!clk_en) state <= IDLE;
+            result <= {1'b0, result_flt[FLOAT_DATA_WIDTH - 2:0]};
+            if(!done) done <= 1'b1;  //adding an additional cycle to compensate for error in NIOS II Platform
+            if (done) state <= IDLE;
 
         end
+
+        default: state <= IDLE;
 
     endcase
 
---
+++ Simulations/a_cordic_tb.v
@@ -0,0 +1,147 @@
+
+`timescale 1 ns / 100 ps
+module a_cordic_tb ();
+
+	//Inputs to DUT are reg type
+	reg [31:0] dataa;
+    wire done;
+	reg clk;
+	reg clk_en;
+
+	//Output from DUT is wire type
+	wire [31:0] result;
+    always
+	    #10 clk = ~clk;
+
+	//Instantiate the DUT
+    //module CORDIC(clk, rst, clk_en, angle_float, result, done);
+	CORDIC unit(
+        .rst            (1'b0),
+        .clk_en         (clk_en),
+        .clk   	        (clk),
+        .angle_float    (dataa),
+		.result         (result),
+        .done  	        (done)
+	);
+
+	// ---- If a clock is required, see below ----
+	// //Create a 50MHz clock
+	// always
+	// 	#10 clk = ~clk;
+	// -----------------------
+
+	//Initial Block
+	initial
+	begin
+		$display($time, " << Starting Simulation >> ");
+		
+		// intialise/set input
+		clk = 1'b0;
+		clk_en <= 1'b1;
+		
+		// If using a clock
+
+		@(posedge clk); 
+		                               
+
+		dataa <= 32'b00111110001010001111010111000011; // x = 0.165 => 00111111011111001000010111101010 = 0x3f7c85ea
+
+		#550
+		clk_en <= 1'b0;
+		#100
+		clk_en <= 1'b1;
+		dataa <= 32'b00111111000010001011010000111001; //x = 0.534 => 00111111010110101110100110011111 = 0x3F5AE99F
+
+		#550
+		clk_en <= 1'b0;
+		#100
+		clk_en <= 1'b1;
+		dataa <= 32'b00111111000010111000010100011111; //x = 0.545 => 00111111010110101110100110011111 = 0x3F5AE99F
+		//outputs cesult <= cos(0.545) = 0.85512729078 = 0.110110101110100110011
+
+
+
+		#10
+		$display($time, "<< Simulation Complete >>");
+		$stop;
+	end
+
+endmodule
+
+
+
+/*
+`timescale 1 ns / 100 ps
+module a_cordic_tb ();
+
+	//Inputs to DUT are reg type
+	reg [31:0] dataa;
+    wire done;
+	reg clk;
+	reg clk_en;
+
+	//Output from DUT is wire type
+	wire [31:0] result;
+    always
+	    #10 clk = ~clk;
+
+	//Instantiate the DUT
+    //module CORDIC(clk, rst, clk_en, angle_float, result, done);
+	CORDIC unit(
+        .rst            (1'b0),
+        .clk_en         (clk_en),
+        .clk   	        (clk),
+        .angle_float    (dataa),
+		.result         (result),
+        .done  	        (done)
+	);
+
+	// ---- If a clock is required, see below ----
+	// //Create a 50MHz clock
+	// always
+	// 	#10 clk = ~clk;
+	// -----------------------
+
+	//Initial Block
+	initial
+	begin
+		$display($time, " << Starting Simulation >> ");
+		
+		// intialise/set input
+		clk = 1'b0;
+		clk_en <= 1'b1;
+		
+		// If using a clock
+		@(posedge clk); 
+		                               
+
+		dataa <= 32'b00111110001010001111010111000011; // x = 0.165 => 00111111011111001000010111101010 = 0x3f7c85ea
+
+		// #1000
+		// clk_en <= 1'b0;
+		// #1000
+		// clk_en <= 1'b1;
+		
+
+		// #1000
+		// dataa <= 32'b00111111000010001011010000111001; //x = 0.534 => 00111111010110101110100110011111 = 0x3F5AE99F
+
+		// #1000
+		// clk_en <= 1'b0;
+		// #1000
+		// clk_en <= 1'b1;
+
+		// #100
+		// dataa <= 32'b00111111000010111000010100011111; //x = 0.545 => 00111111010110101110100110011111 = 0x3F5AE99F
+		// //outputs cesult <= cos(0.545) = 0.85512729078 = 0.110110101110100110011
+
+
+
+		#10
+		$display($time, "<< Simulation Complete >>");
+		$stop;
+	end
+
+endmodule
+
+*/
