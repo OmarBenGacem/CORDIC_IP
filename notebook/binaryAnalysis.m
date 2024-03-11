86416c35d2716b6c263ff4d69da258c0f1e4018c
OmarBenGacem <omar_ben-gacem@imperial.ac.uk>
2024-03-11 12:02:30 +0000

    adsf

--- notebook/binaryAnalysis.m
+++ notebook/binaryAnalysis.m
@@ -10,17 +10,36 @@ data = double_to_fixed(test_num, INT_BITS, FRAC_BITS);
 % disp(bitshift(data, -1))
 % disp(bin(bitshift(data, -1)))
 
+a_cordic_angles_degrees_float = [45.0, 26.56505117707799, 14.036243467926479, 7.125016348901798, 3.576334374997351, 1.7899106082460694, 0.8951737102110744, 0.4476141708605531, 0.22381050036853808, 0.1119056770662069, 0.055952891893803675, 0.027976452617003676, 0.013988227142265016, 0.006994113675352919, 0.003497056850704011, 0.0017485284269804495, 0.0008742642136937803, 0.00043713210687233457, 0.00021856605343934784, 0.00010928302672007149, 5.464151336008544e-05, 2.7320756680048934e-05, 1.3660378340025243e-05, 6.830189170012719e-06, 3.4150945850063712e-06, 1.7075472925031871e-06, 8.537736462515938e-07, 4.2688682312579694e-07, 2.1344341156289847e-07, 1.0672170578144923e-07, 5.336085289072462e-08, 2.668042644536231e-08, 1.3340213222681154e-08, 6.670106611340577e-09, 3.3350533056702886e-09, 1.6675266528351443e-09, 8.337633264175721e-10, 4.1688166320878607e-10, 2.0844083160439303e-10, 1.0422041580219652e-10];
+a_cordic_angles_radians_float = deg2rad(a_cordic_angles_degrees_float);
 
+
+
+%disp(a_cordic_angles_radians_float);
 
 test = 42.0;
 actual = cos(deg2rad(test));
 
-experimental = CORDIC(deg2rad(test), INT_BITS, FRAC_BITS, CORDIC_DEPTH);
+%experimental = CORDIC(deg2rad(test), INT_BITS, FRAC_BITS, CORDIC_DEPTH);
 disp(newline);
-disp(actual);
-disp(experimental);
-errorMessage = ["Error: ", string(double(experimental - actual))];
-disp(join(errorMessage, " "));
+%disp(actual);
+%disp(experimental);
+%errorMessage = ["Error: ", string(double(experimental - actual))];
+%disp(join(errorMessage, " "));
+
+
+
+
+fpr = fipref;
+fpr.NumberDisplay = 'bin';
+d = fi(a_cordic_angles_radians_float,1,22,21);
+disp(d);
+
+
+
+
+
+
 
 
 
