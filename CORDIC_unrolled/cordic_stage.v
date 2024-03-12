861287f5b0527fd6c1940930e17f7b084ece5ad5
OmarBenGacem <omar_ben-gacem@imperial.ac.uk>
2024-03-12 12:33:42 +0000

    added new cordic design

--- CORDIC_unrolled/cordic_stage.v
+++ CORDIC_unrolled/cordic_stage.v
@@ -1,5 +1,87 @@
-module cordic_stage();
+module cordic_stage(clk, clk_en, target, shift_value, shift_angle, angle, x, y, new_angle, new_x, new_y, target_out);
 
+parameter INTEGER_WIDTH = 2;
+parameter DECIMAL_WIDTH = 20;
+parameter DATA_WIDTH = DECIMAL_WIDTH + INTEGER_WIDTH;
+parameter CORDIC_COUNTER_WIDTH = 4;
+
+
+input                                       clk;
+input                                       clk_en;
+input       [DATA_WIDTH - 1 : 0]            target;
+input       [CORDIC_COUNTER_WIDTH - 1 : 0]  shift_value;
+input       [DATA_WIDTH - 1 : 0]            shift_angle;
+input       [DATA_WIDTH - 1 : 0]            angle;
+input       [DATA_WIDTH - 1 : 0]            x;
+input       [DATA_WIDTH - 1 : 0]            y;
+output reg  [DATA_WIDTH - 1 : 0]            new_angle;
+output reg  [DATA_WIDTH - 1 : 0]            new_x;
+output reg  [DATA_WIDTH - 1 : 0]            new_y;
+output reg  [DATA_WIDTH - 1 : 0]            target_out; //used so CORDIC values in the pipeline have memory of their target
+
+wire signed  [CORDIC_DATA_WIDTH - 1: 0]     shifted_y;
+wire signed  [CORDIC_DATA_WIDTH - 1: 0]     shifted_x;
+wire signed  [CORDIC_DATA_WIDTH - 1: 0]     computed_y;
+wire signed  [CORDIC_DATA_WIDTH - 1: 0]     computed_x;
+wire signed  [CORDIC_DATA_WIDTH - 1: 0]     computed_angle;
+wire                                        angle_equal_target;
+wire                                        angle_greater_target;
+
+
+
+Fixed_Add_Sub_signed addsub_x (
+	.dataa   ( x ),
+	.datab   ( shifted_y ),
+    .add_sub ( (angle_greater_target) ), //1 for add, 0 for sub
+	.result  ( computed_x )
+);
+
+Fixed_Add_Sub_signed addsub_y (
+	.dataa   ( y ),
+	.datab   ( shifted_x ),
+    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
+	.result  ( computed_y )
+);
+
+Fixed_Add_Sub_signed addsub_angle (
+	.dataa   ( angle ),
+	.datab   ( shift_value ),
+    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
+	.result  ( computed_angle )
+);
+
+Fixed_Point_gt_signed angle_gt (
+	.dataa   ( angle ),
+	.datab   ( target ),
+	.aeb     ( angle_equal_target ),
+	.agb     ( angle_greater_target )
+);
+
+
+initial begin
+
+    new_angle <= 22'b0;
+    new_x <= 22'b0;
+    new_y <= 22'b0;
+
+end
+
+assign shifted_y = y >>> shift_value;
+assign shifted_x = x >>> shift_value;
+
+always @(posedge clk) begin
+    if (clk_en) begin
+        new_angle <= computed_angle;
+        new_x <= computed_x;
+        new_y <= computed_y;
+        target_out <= target;
+	end else begin
+		// if no clk_en, disable the pipeline
+		new_angle <= 22'b0;
+		new_x <= 22'b0;
+		new_y <= 22'b0;
+	end
+end
 
 
 endmodule
--- CORDIC_unrolled/cordic_unrolled.v
+++ CORDIC_unrolled/cordic_unrolled.v
@@ -15,6 +15,7 @@ parameter x_default = 22'b0010011011011101001110; //20 fractional bits
 //Latency Parameters
 parameter CONVERSION_LATANCY = 4;
 parameter CONVERSION_LATANCY_REV = 4;
+parameter CORDIC_LATENCY = 18;
 
 //Control Parameters
 parameter CORDIC_COUNTER_WIDTH = 4;
@@ -43,31 +44,27 @@ output reg [FLOAT_DATA_WIDTH - 1:0] result;
 
 
 //Control Registers
-reg  [COUNTER_WIDTH - 1:0]              counter_max;
-reg  [CORDIC_COUNTER_WIDTH - 1: 0]      cordic_counter;
-reg  [STATE_WIDTH - 1: 0]               state;
-reg                                     start_conversion;       
-reg                                     delay_reset;
+reg  [COUNTER_WIDTH - 1:0]                cordic_latency;
+reg                                       start_cordic;
+reg                                       cordic_reset;
+reg  [COUNTER_WIDTH - 1:0]                counter_max;
+reg  [STATE_WIDTH - 1: 0]                 state;
+reg                                       start_conversion;       
+reg                                       delay_reset;
 
 
 //Data Registers (and ROM)
-reg          [CORDIC_DATA_WIDTH - 1:0]    CORDIC_shifts [2**CORDIC_ADDRESS_WIDTH-1: 0]; //values as fixed point
 reg  signed  [CORDIC_DATA_WIDTH - 1: 0]   target;
 reg  signed  [CORDIC_DATA_WIDTH - 1: 0]   working_angle;
 reg  signed  [CORDIC_DATA_WIDTH - 1: 0]   x; 
 reg  signed  [CORDIC_DATA_WIDTH - 1: 0]   y;
 
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   shifted_y;
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   shifted_x;
-wire         [CORDIC_DATA_WIDTH - 1: 0]   shift_value;
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   angle;
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   new_x;
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   new_y;
-wire signed  [CORDIC_DATA_WIDTH - 1: 0]   new_angle;
+
 wire         [FLOAT_DATA_WIDTH - 1:0]     result_flt;
-wire                                      angle_greater_target;
-wire                                      angle_equal_target;
+wire         [FLOAT_DATA_WIDTH - 1:0]     cordic_res;
 wire                                      counter_done;      
+wire                                      cordic_done;    
+wire                                      cordic_reset;
 
 
 //module convert_fp_fixed (aclr, clk_en, clock, dataa, result)/* synthesis synthesis_clearbox = 1 */;
@@ -80,8 +77,6 @@ fp_convert_twos_comp converter_fp_fix (
     .result     ( angle )
 
 );
-
-
 
 FIXED_Convert_twos_comp converter_fix_fp (
     .clock      ( clk ),
@@ -99,35 +94,25 @@ delay stopper (
 		.done ( counter_done )
 );
 
-
-Fixed_Add_Sub_signed addsub_x (
-	.dataa ( x ),
-	.datab ( shifted_y ),
-    .add_sub ( (angle_greater_target) ), //1 for add, 0 for sub
-	.result ( new_x )
+delay stopper_cordic (
+		.max  ( cordic_latency ),
+		.clk  ( clk ),
+		.rst  ( cordic_reset ),
+		.done ( cordic_done )
 );
 
-Fixed_Add_Sub_signed addsub_y (
-	.dataa ( y ),
-	.datab ( shifted_x ),
-    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
-	.result ( new_y )
-);
 
-Fixed_Add_Sub_signed addsub_angle (
-	.dataa ( working_angle ),
-	.datab ( shift_value ),
-    .add_sub ( !(angle_greater_target) ), //1 for add, 0 for sun
-	.result ( new_angle )
-);
+//module cordic_frame (clk, clk_en, rst, target, result);
 
-Fixed_Point_gt_signed angle_gt (
-	.dataa ( working_angle ),
-	.datab ( target ),
-	.aeb ( angle_equal_target ),
-	.agb ( angle_greater_target )
-);
+cordic_frame cordic (
 
+    .clk        ( clk ),
+    .clk_en     ( start_cordic ),
+    .rst        ( rst ),
+    .target     ( angle ),
+    .result     ( cordic_res )
+
+);
 
 
 initial begin
@@ -143,31 +128,13 @@ initial begin
     done <= 1'b0;
     target <= 22'b0;
 
-
-
-    //20 fractional bits, 2 integer bits    
-    CORDIC_shifts[4'b0000] <= 22'b0011001001000011111101;
-    CORDIC_shifts[4'b0001] <= 22'b0001110110101100011001;
-    CORDIC_shifts[4'b0010] <= 22'b0000111110101101101110;
-    CORDIC_shifts[4'b0011] <= 22'b0000011111110101011011;
-    CORDIC_shifts[4'b0100] <= 22'b0000001111111110101010;
-    CORDIC_shifts[4'b0101] <= 22'b0000000111111111110101;
-    CORDIC_shifts[4'b0110] <= 22'b0000000011111111111110;
-    CORDIC_shifts[4'b0111] <= 22'b0000000001111111111111;
-    CORDIC_shifts[4'b1000] <= 22'b0000000000111111111111;
-    CORDIC_shifts[4'b1001] <= 22'b0000000000011111111111;
-    CORDIC_shifts[4'b1010] <= 22'b0000000000001111111111;
-    CORDIC_shifts[4'b1011] <= 22'b0000000000000111111111;
-    CORDIC_shifts[4'b1100] <= 22'b0000000000000011111111;
-    CORDIC_shifts[4'b1101] <= 22'b0000000000000001111111;
-    CORDIC_shifts[4'b1110] <= 22'b0000000000000000111111;
-    CORDIC_shifts[4'b1111] <= 22'b0000000000000000011111;
+    start_cordic <= 1'b0;
+    cordic_reset <= 1'b0;
+    cordic_latency <= CORDIC_LATENCY;
 
 
 end
-assign shifted_y = y >>> cordic_counter;
-assign shifted_x = x >>> cordic_counter;
-assign shift_value = CORDIC_shifts[cordic_counter];
+
 
 
 //clock syncronous signals
@@ -186,6 +153,7 @@ always @(posedge clk) begin
         counter_max <= CONVERSION_LATANCY;
         done <= 1'b0;
         target <= 22'b0;
+        
     end
 
     if (!clk_en) begin
@@ -227,6 +195,8 @@ always @(posedge clk) begin
             delay_reset <= 1'b0;
             if (counter_done) 
             begin state <= CORDIC_MAIN; 
+            start_cordic <= 1'b1;
+            cordic_reset <= 1'b1;
             start_conversion <= 1'b0; 
             target <= angle;
             end
@@ -236,25 +206,16 @@ always @(posedge clk) begin
 
         CORDIC_MAIN: begin //0111
 
-            if (cordic_counter == CORDIC_DEPTH || angle_equal_target) begin
-
+            cordic_reset <= 1'b0;
+            if (cordic_done) begin
+                x <= cordic_res;
+                start_cordic <= 1'b0;
                 delay_reset <= 1'b1; //start delay block
                 start_conversion <= 1'b1;
                 state <= CONVERTING_BACK;
-            end else begin
-                x <= new_x;
-                y <= new_y;
-                working_angle <= new_angle;
-                cordic_counter <= cordic_counter + 1;
-
-                //preempting activating the delays
-                if (cordic_counter == CORDIC_DEPTH - 1) begin
-                    delay_reset <= 1'b1; //start delay block
-                    start_conversion <= 1'b1;
-                    counter_max <= CONVERSION_LATANCY_REV;
-                end
             end
 
+            
         end
 
         CONVERTING_BACK: begin //0011
